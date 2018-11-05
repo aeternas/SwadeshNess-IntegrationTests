eece288c98f9f506d6490cc7a0b119359c8aeeec
Ivan Golikov <igolikov@octoberry.ru>
2018-11-05 19:36:35 +0300

    Updated script

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
+	version := os.Getenv("VERSION")
+	fmt.Println("Expected version is: ", version)
+	if actualVersion := requestVersion(); version != actualVersion {
+		panic("Actual version is not equals to expected")
+	}
 	httpClient := &http.Client{Timeout: time.Second * 15}
 	url := "http://vpered.su:8080/dev/?translate=Hello+World&group=Romanic"
 	fmt.Println("URL:>", url)
@@ -18,6 +25,31 @@ func main() {
 	}
 
 	resp, err := httpClient.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		panic("Invalid response status")
+	}
+}
+
+func requestVersion() string {
+	httpClient := &http.Client{Timeout: time.Second * 15}
+	versionUrl := "http://vpered.su:8080/dev/version"
+	fmt.Println("VERSIONURL:>", versionUrl)
+
+	req, err := http.NewRequest("GET", versionUrl, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := httpClient.Do(req)
+
 	if err != nil {
 		panic(err)
 	}
@@ -26,4 +58,8 @@ func main() {
 	if resp.StatusCode != 200 {
 		panic("Invalid response status")
 	}
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	bodyString := string(bodyBytes)
+
+	return bodyString
 }
