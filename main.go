a27b1f1f2d45977e1daa7a251f2787cb74664a40
Ivan Golikov <igolikov@octoberry.ru>
2018-11-07 22:06:26 +0300

    Fixed expected result

--- main.go
+++ main.go
@@ -100,7 +100,7 @@ func requestTranslation() {
 		log.Fatalf("Error unmarshalling body")
 	}
 
-	if data.Results[0].Results[3].Translation != "Bana" {
+	if data.Results[0].Results[3].Translation != "bana" {
 		log.Fatalf("Result translation doesn't match expected one")
 	}
 }
